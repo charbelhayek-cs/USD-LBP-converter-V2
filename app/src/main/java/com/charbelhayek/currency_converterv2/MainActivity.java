48c2773d2ff607a0281f2053471a00910bc4538e
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-03-25 15:44:25 +0200

    did the intetn function on the button so that when i press the button i will be redirected to the next page

--- app/src/main/java/com/charbelhayek/currency_converterv2/MainActivity.java
+++ app/src/main/java/com/charbelhayek/currency_converterv2/MainActivity.java
@@ -2,7 +2,9 @@ package com.charbelhayek.currency_converterv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToNextPage(View v){
+        Intent intent=new Intent(getApplicationContext(),MainActivity2.class);
+        startActivity(intent);
     }
 }

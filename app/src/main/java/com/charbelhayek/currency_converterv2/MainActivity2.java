cf61011b78bc53bedfa28b1c8bd88445ff18ad0c
samerelearning <samerelearning@outlook.com>
2022-04-03 01:00:27 +0300

    Declaring internet permission

--- app/src/main/java/com/charbelhayek/currency_converterv2/MainActivity2.java
+++ app/src/main/java/com/charbelhayek/currency_converterv2/MainActivity2.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -69,7 +70,7 @@ public class MainActivity2 extends AppCompatActivity {
         protected void onPostExecute(String str)
         {
         //Post-execution of the API
-
+            Log.i("result", str);
         }
    }
     @Override
